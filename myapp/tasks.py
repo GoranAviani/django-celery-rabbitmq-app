decf5afc67277ab2c9c7cf15f398003cbaa7ee3a
goranaviani <goranaviani@gmail.com>
2019-08-24 11:59:21 +0200

    task and view

---
+++ myapp/tasks.py
@@ -0,0 +1,11 @@
+import string
+
+from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
+
+from celery import shared_task
+
+@shared_task
+def print_lot_of_times(someData, someData1):
+    for i in range(someData1):
+        print(someData)
